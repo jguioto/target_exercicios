a13e5616c8a59d1681f9ae94f5897bc21f6403bd
João Guioto <92184462+jguioto@users.noreply.github.com>
2023-04-15 17:23:59 -0300

    Primeiro
    O primeiro exercício foi feito, e o resultado é 91.

---
+++ Primeiro.java
@@ -0,0 +1,10 @@
+public class Primeiro {
+    public static void main(String[] args) {
+        int indice = 13, soma = 0, k = 0;
+        while(k < indice) {
+            k = k + 1;
+            soma = soma + k;
+        }
+        System.out.println(soma); //RESULTADO = 91
+    }
+}
